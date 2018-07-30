3feeaa972429a7fe2c85947006db6e9b693e0d62
marks <marks1024@users.noreply.github.com>
2018-07-30 09:55:23 +0600

    decorator example

--- utiltest/src/main/java/kz/edu/nu/cs/teaching/DecoratorExample.java
+++ utiltest/src/main/java/kz/edu/nu/cs/teaching/DecoratorExample.java
@@ -1,71 +1,71 @@
 package kz.edu.nu.cs.teaching;
 
 public class DecoratorExample {
-	public static void main(String[] args) {
-		TextComponent tx = new TextBase("The Text!");
-		System.out.println(tx.getClass().toString());
-		System.out.println(tx.produceText());
+    public static void main(String[] args) {
+        TextComponent tx = new TextBase("The Text!");
+        System.out.println(tx.getClass().toString());
+        System.out.println(tx.produceText());
 
-		tx = new DashBorderDecorator(tx);
-		System.out.println(tx.getClass().toString());
-		System.out.println(tx.produceText()); 
-		
-		tx = new CapitalDecorator(tx);
-	    System.out.println(tx.getClass().toString());
-	    System.out.println(tx.produceText());
-	}
+        tx = new DashBorderDecorator(tx);
+        System.out.println(tx.getClass().toString());
+        System.out.println(tx.produceText());
+
+        tx = new CapitalDecorator(tx);
+        System.out.println(tx.getClass().toString());
+        System.out.println(tx.produceText());
+    }
 }
 
 abstract class TextComponent {
-	abstract public String produceText();
+    abstract public String produceText();
 }
 
 abstract class TextDecorator extends TextComponent {
-	protected TextComponent next;
+    protected TextComponent next;
 
-	public TextDecorator(TextComponent t) {
-		this.next = t;
-	}
+    public TextDecorator(TextComponent t) {
+        this.next = t;
+    }
 
-	public String produceText() {
-		return this.next.produceText();
-	}
+    public String produceText() {
+        return this.next.produceText();
+    }
 }
 
 class TextBase extends TextComponent {
-	private String s;
+    private String s;
 
-	public TextBase(String s) {
-		this.s = s;
-	}
+    public TextBase(String s) {
+        this.s = s;
+    }
 
-	@Override
-	public String produceText() {
-		return s;
-	}
+    @Override
+    public String produceText() {
+        return s;
+    }
 
 }
 
 class DashBorderDecorator extends TextDecorator {
 
-	public DashBorderDecorator(TextComponent t) {
-		super(t);
-	}
+    public DashBorderDecorator(TextComponent t) {
+        super(t);
+    }
 
-	@Override
-	public String produceText() {
-		return "--- " + super.produceText() + " ---";
-	}
+    @Override
+    public String produceText() {
+        return "--- " + super.produceText() + " ---";
+    }
 }
 
 class CapitalDecorator extends TextDecorator {
 
-	public CapitalDecorator(TextComponent t) {
-		super(t);
-	}
+    public CapitalDecorator(TextComponent t) {
+        super(t);
+    }
 
-	@Override
-	public String produceText() {
-		return super.produceText().toUpperCase();
-	}
+    @Override
+    public String produceText() {
+        return super.produceText().toUpperCase();
+    }
 }
