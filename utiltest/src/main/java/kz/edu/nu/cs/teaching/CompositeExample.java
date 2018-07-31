3873f02d9ecc50c9439940779da32636761dafbc
marks <marks1024@users.noreply.github.com>
2018-07-31 10:34:50 +0600

    composite pattern example

---
+++ utiltest/src/main/java/kz/edu/nu/cs/teaching/CompositeExample.java
@@ -0,0 +1,58 @@
+package kz.edu.nu.cs.teaching;
+
+public class CompositeExample {
+  public static void main(String[] args) {
+    Multiplying x = new Multiplying(new Adding(new Vals(2.0), new Vals(2.0)),
+        new Adding(new Vals(2.0), new Multiplying(new Vals(2.0), new Vals(2.0))));
+    
+    System.out.println(x.getValue());
+  }
+}
+
+abstract class Term {
+  public abstract double getValue();
+}
+
+class Vals extends Term {
+  private double myValue;
+
+  public Vals(double m) {
+    this.myValue = m;
+  }
+
+  public double getValue() {
+    return myValue;
+  }
+}
+
+abstract class Opers extends Term {
+  protected Term leftVal;
+  protected Term rightVal;
+
+  public Opers(Term l, Term r) {
+    this.leftVal = l;
+    this.rightVal = r;
+  }
+}
+
+class Adding extends Opers {
+  public Adding(Term l, Term r) {
+    super(l, r);
+  }
+
+  @Override
+  public double getValue() {
+    return this.leftVal.getValue() + this.rightVal.getValue();
+  }
+}
+
+class Multiplying extends Opers {
+  public Multiplying(Term l, Term r) {
+    super(l, r);
+  }
+
+  @Override
+  public double getValue() {
+    return this.leftVal.getValue() * this.rightVal.getValue();
+  }
+}
