7a6205787903a72efc0ccea1e9c8f0b699cbf814
marks <marks1024@users.noreply.github.com>
2018-08-02 13:09:39 +0600

    updates

--- current-maven-webapp-project/src/main/java/kz/edu/nu/cs/MyServlet.java
+++ current-maven-webapp-project/src/main/java/kz/edu/nu/cs/MyServlet.java
@@ -8,40 +8,48 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class MyServlet
+ * Servlet implementation class MyServlet.
  * 
- * Access at url <code>hostname/current-maven-webapp-project/myservlet</code>
+ * This class is generated as part of the current-maven-webapp-project
+ * archetype. This archetype generates a maven project with some more up-to-date
+ * features compared with the org.apache quickstart webapp archetype.
+ * 
+ * Access at url <code>hostname/...projectname.../myservlet</code>
+ * 
+ * Some of the updates included in this archetype
+ * <ul>
+ * <li>Version 4.12 of JUnit
+ * <li>Build with Java 8
+ * <li>Servlet API 3.0.1
+ * </ul>
+ * <p>
+ * If you have obtained this project then you can build and install to your
+ * local maven repository. To build the project, go to the project root and run
+ * <code>mvn archetype:create-from-project</code>.
+ * <p>
+ * After building, navigate to <code>target/generated-sources/archetype</code>
+ * and run <code>mvn install</code>. This will add the archetype to your local
+ * maven repository.
+ * <p>
+ * Add <code>.m2/archetype-catalog.xml</code> as a local catalog in eclipse
  * 
  * @author marks1024
  */
 @WebServlet(urlPatterns = { "/myservlet" })
 public class MyServlet extends HttpServlet {
-  private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-  /**
-   * @see HttpServlet#HttpServlet()
-   */
-  public MyServlet() {
-    super();
-    // TODO Auto-generated constructor stub
-  }
+    public MyServlet() {
+        super();
+    }
 
-  /**
-   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-   *      response)
-   */
-  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    // TODO Auto-generated method stub
-    response.getWriter().append("Served at: ").append(request.getContextPath());
-  }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.getWriter().append("Served at: ").append(request.getContextPath());
+    }
 
-  /**
-   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-   *      response)
-   */
-  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    // TODO Auto-generated method stub
-    doGet(request, response);
-  }
-
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
 }
